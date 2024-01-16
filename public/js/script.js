2fcc87ec4b67fffc6335336cbb3f2ae82183cdfc
ekaputra04 <ekaputrajuniawan@gmail.com>
2024-01-17 01:50:13 +0800

    add autoplaying music

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,3 @@
-// hamburger
-
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
@@ -14,9 +12,42 @@ window.onscroll = function () {
   }
 };
 
+// hamburger
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#nav-menu");
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
+});
+
+// music
+document.addEventListener("DOMContentLoaded", function () {
+  // Mendapatkan elemen audio
+  var audio = document.getElementById("myAudio");
+
+  // Mendapatkan elemen tombol pause
+  var pauseButton = document.getElementById("pauseButton");
+
+  // Mendapatkan elemen ikon
+  var icon = document.getElementById("icon");
+
+  // Menambahkan event listener untuk tombol pause
+  pauseButton.addEventListener("click", function () {
+    if (audio.paused) {
+      // Jika sedang di-pause, play
+      audio.play();
+      icon.classList.remove("bi-play-circle");
+      icon.classList.add("bi-pause-circle");
+    } else {
+      // Jika sedang bermain, pause
+      audio.pause();
+      icon.classList.remove("bi-pause-circle");
+      icon.classList.add("bi-play-circle");
+    }
+  });
+
+  // Menambahkan event listener untuk memutarkan audio saat sudah dapat dimainkan
+  audio.addEventListener("canplay", function () {
+    audio.play();
+  });
 });
